9face7dec8b97a23d5f24173ab9cfeb0591ed789
xji3 <xji3@ncsu.edu>
2021-10-10 13:37:13 -0500

    edite script

--- git_repos/fetch.py
+++ git_repos/fetch.py
@@ -23,9 +23,21 @@ if __name__ == "__main__":
         "Paul-Pluscht"
     ]
 
+    git_repos = os.getcwd()
     git_link = "https://github.com/tulane-math-7360-2021/Math-7360-XXXX-XXXX.git"
-    for student_name in student_names:
-        git_repo = './Math-7360-' + student_name
+    for student_name in student_names[1:]:
+        git_repo = git_repos + '/Math-7360-' + student_name
         if not os.path.isdir(git_repo):
             git_clone_cmd = 'git clone ' + git_link.replace("XXXX-XXXX", student_name)
             os.system(git_clone_cmd)
+        else:
+            git_pull_cmd = 'git pull'
+            os.chdir(git_repo)
+            os.system(git_pull_cmd)
+
+        commit_cmd = "git commit -m 'add hw1 grade'"
+        os.chdir(git_repo)
+        os.system('pwd')
+        os.system(commit_cmd)
+        os.system('git push')
+
